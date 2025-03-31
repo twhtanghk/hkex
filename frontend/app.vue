0ea955b90251fb153d29fcda3b081a3376f451c0
root <twhtanghk@gmail.com>
2025-03-31 23:26:40 +0800

    add nuxt ui to format news data

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,20 @@
 <template>
-  <div v-for='i in msgs'>
-    {{JSON.stringify(i)}}
-  </div>
+  <UApp>
+    <NuxtPage>
+      <UTable sticky :data="msgs" :columns="columns">
+        <template #code-cell="{row}">
+	  <ULink :to="chart(row)" target='blank'>
+	    {{row.original.code}}
+	  </ULink>
+	</template>
+        <template #type-cell="{row}">
+          <ULink :to="url(row.original.link)" target='blank'>
+	    {{row.original.type}}
+	  </ULink>
+	</template>
+      </UTable>
+    </NuxtPage>
+  </UApp>
 </template>
 
 <script setup>
@@ -11,6 +24,24 @@ import {HKEXNew, HKEXList, reverse} from '../index.js'
 let hkex = new HKEXNew()
 
 const msgs = reactive([])
+const columns = [
+  { accessorKey: 'code', header: 'Code' },
+  { accessorKey: 'name', header: 'Name' },
+  { accessorKey: 'type', header: 'Type' },
+  { accessorKey: 'typeDetail', header: 'Detail' },
+  { accessorKey: 'title', header: 'Title' },
+  { accessorKey: 'releasedAt', header: 'Date' }
+]
+
+function chart(row) {
+  let code = Number(row.original.code).toString()
+  return `https://www.tradingview.com/chart/?symbol=${code}`
+}
+
+function url(path) {
+  let a = URL.parse(HKEXNew.url({page: 0}))
+  return a.origin + path
+}
 
 function match({typeDetail, type, title}) {
   let pattern = new RegExp(process.env.ALERT)
@@ -26,3 +57,8 @@ async function news() {
 
 await news()
 </script>
+
+<style scoped>
+@import "tailwindcss";
+@import "@nuxt/ui";
+</style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -11,5 +11,20 @@ export default defineNuxtConfig({
        include: ['../index.js', /node_modules/],
         transformMixedEsModules: true
     }
-  }
+  },
+  /*
+  experimental: {
+    clientNodeCompat: true
+  },
+  vite: {
+    define: {
+      "process.version": "navigator.userAgent",
+      "process.platform": "navigator.userAgent",
+      "process.arch": "navigator.userAgent",
+      "stream.PassThrough": "TransformStream"
+    }
+  },
+  target: 'static',
+  ssr: false
+  */
 })
